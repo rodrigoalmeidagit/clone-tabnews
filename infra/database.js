9284eff99979c6b192a16413583640f446f06385
Rodrigo Almeida <rodrigoalmeidagit@gmail.com>
2023-11-19 20:07:15 -0300

    move `.env` file to `.env.development`

--- infra/database.js
+++ infra/database.js
@@ -6,15 +6,15 @@ async function query(queryObject) {
     port: process.env.POSTGRES_PORT,
     user: process.env.POSTGRES_USER,
     database: process.env.POSTGRES_DB,
-    password: process.env.POSTGRES_PASSWORD 
+    password: process.env.POSTGRES_PASSWORD
   });
   await client.connect();
   const result = await client.query(queryObject);
   client.end();
   return result;
-} 
+}
 
 export default {
   query: query,
-}  
+}
 
